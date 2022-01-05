5dcf56117ad9c8aefd7b2189356332aa35ad5f30
Cristian Olivares <cri_jair@hotmail.com>
2022-01-05 08:28:23 -0600

    Agregar archivos de proyecto.

---
+++ BarCode/BarCode/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using BarCode.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarCode
+{
+    
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        IList<Codigo> people = new ObservableCollection<Codigo>();
+        
+        public MainPage()
+        {
+            BindingContext = people;
+            InitializeComponent();
+        }
+
+        
+
+
+        private async void BtnScan_Clicked(object sender, EventArgs e)
+        {
+            try{
+                var scanner = DependencyService.Get<ScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null) 
+                {
+                    TxtBarcode.Text = result;
+                    people.Add(new Codigo(result));
+                }
+            } catch (Exception ex) { throw; }
+        }
+
+        private async void BtnBorrar_Clicked(object sender, EventArgs e)
+        {
+            people.Clear();
+        }
+
+
+    }
+}
---
+++ BarCode/BarCode/Services/ScanningService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarCode.Services
+{
+    public interface ScanningService
+    {
+        Task<string> ScanAsync();
+    }
+}
